24a838c1adb9a441d7d2abf28fefd8d02f6a76de
Marco Michl <marco.michl@carissma.eu>
2025-02-25 21:27:24 +0100

    added simple parsing and some util functions for first method

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,42 @@
-use std::env;
-
 mod xacml;
+mod utils;
+
+use std::env;
+use utils::{parse_xml_file, serialize_to_xml_file};
+use xacml::structs::RequestType;
 
 fn main() {
-    fn main() {
-        let args: Vec<String> = env::args().collect();
+ 
+    println!("XACML 3.0 Rust implementation");
+    let args: Vec<String> = env::args().collect();
 
-        if args.len() < 2 {
-            eprintln!("Usage: {} <command> [<args>]", args[0]);
-            return;
+    if args.len() < 2 {
+        eprintln!("Usage: {} <command> [<args>]", args[0]);
+        return;
+    }
+
+    match args[1].as_str() {
+        "decideRequest" => {
+            if args.len() != 4 {
+                eprintln!("Usage: {} evaluate <policy> <context>", args[0]);
+                return;
+            }
+            let policy = &args[2];
+            let context_id = &args[3];
+            decide_requets(policy, context_id);
         }
-
-        match args[1].as_str() {
-            "evaluate" => {
-                if args.len() != 4 {
-                    eprintln!("Usage: {} evaluate <policy> <context>", args[0]);
-                    return;
-                }
-                let policy = &args[2];
-                let context = &args[3];
-                evaluate_policy(policy, context);
-            }
-            _ => {
-                eprintln!("Unknown command: {}", args[1]);
-            }
+        _ => {
+            eprintln!("Unknown command: {}", args[1]);
         }
     }
+}
 
     fn evaluate_policy(policy: &str, context: &str) {
         // Placeholder for actual policy evaluation logic
         println!("Evaluating policy: {} with context: {}", policy, context);
     }
-}
+
+    fn decide_requets(request_file: &str, context_id: &str) {
+        let request = parse_xml_file::<RequestType>(request_file)
+            .expect("Failed to parse request file");
+    }
---
+++ src/utils.rs
@@ -0,0 +1,56 @@
+use std::{fmt::{self, Display, Formatter, Debug}, fs};
+
+use quick_xml::{de::from_str, se::to_string};
+use serde::{Deserialize, Serialize};
+
+pub fn parse_xml_file<T>(path: &str) -> Result<T, XacmlError>
+where
+    T: for<'de> Deserialize<'de>,
+{
+    let content = fs::read_to_string(path)
+        .map_err( |_| XacmlError::new(XacmlErrorType::IoError, format!("Error reading file: {}", path)))?;
+    from_str::<T>(&content)
+        .map_err( |_| XacmlError::new(XacmlErrorType::DeserializeError, format!("Error deserializing string: {}", content)))
+}
+
+pub fn serialize_to_xml_file<T>(data: &T, path: &str) -> Result<(), XacmlError>
+where
+    T: Serialize + Debug,
+{
+    let string = to_string(data)
+        .map_err( |_| XacmlError::new(XacmlErrorType::SerializeError, format!("Error serializing data: {:?}", data)))?;
+    fs::write(path, &string)
+        .map_err( |_| XacmlError::new(XacmlErrorType::IoError, format!("Error writing to file: {}", path)))?;
+    Ok(())
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum XacmlErrorType{
+    IoError,
+    DeserializeError,
+    SerializeError,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct XacmlError {
+    error_type: XacmlErrorType,
+    message: String,
+}
+
+impl XacmlError {
+    pub fn new(error_type: XacmlErrorType, message: String) -> Self {
+        XacmlError {
+            error_type,
+            message,
+        }
+    }
+}
+
+impl Display for XacmlError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "XacmlError: {:?} - {}", self.error_type, self.message)
+    }
+}
+
+
+
